93df1bff4925dbebb7480490e668aeec1dbcb6a3
You Heng Zhou <60205850+youhengzhou@users.noreply.github.com>
2021-06-13 19:52:06 -0400

    Add files via upload

---
+++ engine/char/char.py
@@ -0,0 +1,31 @@
+default_hero = {
+    'name': 'default',
+    'hpt': 6,
+    'atk': 6,
+    'def': 6,
+    'str': 6,
+    'wgt': 6,
+    'mov': 6,
+}
+
+fantasia_linche = {
+    'name': 'Marina Bordichels',
+    'alter-ego': 'Linche Sebastian',
+    'hpt': 4,
+    'atk': 6,
+    'def': 8,
+    'str': 3,
+    'wgt': 2,
+    'mov': 7,
+    'skills': {
+        'silver-tongue': 'speech 18/1d20',
+        'legerdemain': 'steal 20/1d20',
+        'open lock': 'steal 20/1d20',
+        'dagger mastery': 'steal 20/1d20',
+        'inhuman agility': 'steal 20/1d20',
+        'summon temporary double': 'steal 20/1d20',
+        'teleport object (limited distance)': 'steal 20/1d20',
+        'mass hallucination': 'steal 20/1d20',
+        'group invisibility': 'steal 20/1d20',
+    }
+}
