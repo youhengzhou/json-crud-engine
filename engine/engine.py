5a9a627c15cbcbba200b3ee4df190cfd03de7e37
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2021-06-21 13:48:13 -0400

    update to 2.0

--- engine/engine.py
+++ engine/engine.py
@@ -1,51 +1,83 @@
 import os
 import json
+import random
+import names
+
+# player      the player name
+# atr         the default attributes
+# play        displays the generic information
+# inputAtr    logic to set the attributes, taking a string and a number and put it to the dictionary
+# outputAtr   returns the atr by getting a string and reading from the atr dict
+
+player = 'default'
+def play():
+    print(
+    '''
+CYOA ENGINE
+Welcome: {name}
+    '''.format(name=player))
 
 path = os.getcwd() + '\\database\\'
 
 # CRUD
-def create(dict,str):
-    with open(path + str + '.json', 'w') as outfile:
-        json.dump(dict, outfile)
-
-def create_blank(str):
-    with open(path + str + '.json', 'w') as outfile:
-        json.dump(outfile)
+def create(dictionary,string):
+    with open(path + string + '.json', 'w') as outfile:
+        json.dump(dictionary, outfile, indent=4)
     
-def retrieve(str):
-    with open(path + str + '.json', 'r') as f:
+def retrieve(string):
+    with open(path + string + '.json', 'r') as f:
         return(json.load(f))
 
-def update(dict,str):
-    with open(path + str + '.json', 'r') as f:
+def update(dictionary,string):
+    with open(path + string + '.json', 'r') as f:
         data=(json.load(f))
-        data.update(dict)
-        with open(path + str + '.json', 'w') as outfile:
-            json.dump(dict, outfile)
+        data.update(dictionary)
+    with open(path + string + '.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
 
-def delete(str):
-    if os.path.exists(path + str + '.json'):
-        os.remove(path + str + '.json')
+def delete(string):
+    if os.path.exists(path + string + '.json'):
+        os.remove(path + string + '.json')
     else:
         print('The selected file does not exist')
 
 # IEO
-def input_atr(dict,str):
-    for i in range(len(str)):
-            if str[i] == " ":
-                word1=str[:i]
-                word2=str[i+1:]
+def input_atr(dictionary,string):
+    for i in range(len(string)):
+            if string[i] == " ":
+                word1=string[:i]
+                word2=string[i+1:]
                 print('i ' + word1)
                 print('i ' + word2)
-                dict[word1] = word2
-                return(dict)
+                dictionary[word1] = word2
+                return(dictionary)
 
-def erase_atr(dict,str):
-    print('e ' + str)
-    print('e ' + dict.pop(str,'atr_not_found'))
-    return(dict)
+def erase_atr(dictionary,string):
+    print('e ' + string)
+    print('e ' + dictionary.pop(string,'atr_not_found'))
+    return(dictionary)
 
-def output_atr(dict,str):
-    print('o '+ str)
-    print('o ' + dict[str])
-    return(dict[str])
+def output_atr(dictionary,string):
+    print('o '+ string)
+    print('o ' + dictionary[string])
+    return(dictionary[string])
+
+# CYOA
+# on an engine level, create a copy of the dictionary as the dict_template
+def generate(dict_input):
+    dict_output=dict(dict_input)
+    keylist=[]
+    for key in dict_input:
+        keylist.append(key)
+    for i in range(0, len(dict_input), 1):
+        r = random.randint(0, len(dict_input[keylist[i]])-1)
+        dict_output[keylist[i]] = dict_input[keylist[i]][r]
+    return(dict_output)
+
+# generate names
+def mname():
+    return(names.get_full_name(gender='male'))
+
+def fname():
+    return(names.get_full_name(gender='female'))
+    
